267fc6fd2aef7e3185c721874cbbfe30a51b31cb
Negzaoui <negzaouioussama15@gmail.com>
2026-05-09 22:56:13 +0100

    supp-09-05-2026

--- test-regex.js
+++
@@ -1,3 +0,0 @@
-const regex = /^https?:\/\/(?:www\.)?github\.com\/([^/]+)\/([^/]+?)(?:\/tree\/([^/]+))?(?:\/.*)?(?:\.git)?$/i;
-const url = 'https://github.com/Souhaila4/arenaofcodersfrontend-mobile/tree/kaboura';
-console.log(url.match(regex));
